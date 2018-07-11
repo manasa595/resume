db8e033ba6a4c0663fa9be512d0e156bbe6bebab
manasa595 <40975887+manasa595@users.noreply.github.com>
2018-07-11 15:43:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -33,6 +33,8 @@ newFile.then(data=>{
   console.log(data);
   career(data.career);
   education(data.education);
+  skills(data.skills);
+  achivements(data.achievements);
 })
 
 var childTwo=document.querySelector(".childtwo");
@@ -47,7 +49,6 @@ function career(careerObj){
   careerP.textContent=careerObj.info;
   childTwo.appendChild(careerP);
 }
-
 function education(edu){
   var careerHeading=document.createElement("h2");
   careerHeading.textContent="Education Qualification";
@@ -59,14 +60,50 @@ for(var i=0;i<edu.length;i++){
   eduH3=document.createElement("h3");
   eduH3.textContent=edu[i].degree;
   childTwo.appendChild(eduH3);
-  var eduUl=document.createElement("Ul");
+  var eduU1=document.createElement("Ul");
   var eduLi=document.createElement("li");
   eduLi.textContent=edu[i].institute;
-  eduUl.appendChild(eduLi);
-  childTwo.appendChild(eduUl);
-  var eduLi2=document.createElement("li2")
-  eduLi2=textContent=edu[i].data;
-
+  eduU1.appendChild(eduLi);
+  childTwo.appendChild(eduU1);
 }
+}
+function skills(skillinfo){
+  var careerHeading=document.createElement("h2");
+  careerHeading.textContent="Technical skills";
+  childTwo.appendChild(careerHeading);
+  var hr=document.createElement("hr");
+  careerHeading.appendChild(hr);
+var skillTable=document.createElement("table");
+skillTable.border="1";
+childTwo.appendChild(skillTable)
+var tableData="";
+for(var i=0;i<skillinfo.length;i++){
+  tableData+="<tr><td>"+skillinfo[i].title+" </td><td>"+skillinfo[i].info+"</td></tr>";
+}
+skillTable.innerHTML=tableData;
+}
+function achivements(archi){
+  var achivementsHeading=document.createElement("h2");
+  achivementsHeading.textContent="achivements";
+  childTwo.appendChild(achivementsHeading);
+  var hr=document.createElement("hr");
+  achivementsHeading.appendChild(hr);
+for(var i=0;i<archi.length;i++){
 
+  var archiU1=document.createElement("Ul");
+  var archiLi=document.createElement("li");
+  archiLi.textContent=archi[i].info;
+  archiU1.appendChild(archiLi);
+  childTwo.appendChild(archiU1);
+// var archiU2=document.createElement("Ul");
+// var archiLi1=document.createElement("li");
+// archiLi1.textContent=archi[i].co-circular;
+// archiU2.appendChild(archiLi2);
+// childTwo.appendChild(archiU2);
+// var archiU3=document.createElement("Ul");
+// var archiLi2=document.createElement("li");
+// archiLi2.textContent=edu[i].training;
+// archiU3.appendChild(archiLi2);
+// childTwo.appendChild(archiU3);
+}
 }
